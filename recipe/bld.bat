6459edb03728219e4e229b9da59d42d6cd91345f
Uwe L. Korn <uwe.korn@quantco.com>
2024-10-15 14:22:50 +0200

    Overhaul recipe

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,17 @@
 @echo on
 
+set CARGO_PROFILE_RELEASE_STRIP=symbols
+set CARGO_PROFILE_RELEASE_LTO=fat
+
 :: build
 cargo install --locked ^
+    --no-track ^
     --root "%PREFIX%" ^
     --path . ^
     || exit 1
-
-:: strip debug symbols
-strip "%PREFIX%\bin\cargo-llvm-cov.exe" || exit 1
 
 :: dump licenses
 cargo-bundle-licenses ^
     --format yaml ^
     --output "%SRC_DIR%\THIRDPARTY.yml" ^
     || exit 1
-
-:: remove extra build files
-del /F /Q "%PREFIX%\.crates.toml"
-del /F /Q "%PREFIX%\.crates2.json"
--- recipe/build.sh
+++ recipe/build.sh
@@ -4,18 +4,15 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export RUST_BACKTRACE=1
 export OPENSSL_DIR=$PREFIX
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
 
 # build
 cargo install --locked \
+    --no-track \
     --root "$PREFIX" \
     --path .
-
-# strip debug symbols
-"$STRIP" "$PREFIX/bin/cargo-llvm-cov"
 
 cargo-bundle-licenses \
     --format yaml \
     --output "${SRC_DIR}/THIRDPARTY.yml"
-
-# remove extra build files
-rm -f "${PREFIX}/.crates2.json" "${PREFIX}/.crates.toml"
